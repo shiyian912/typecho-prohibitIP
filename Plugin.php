dd40fab34c921afc2824ab6b60c89da970e42150
shiyian912 <69504416+shiyian912@users.noreply.github.com>
2023-03-28 10:35:52 +0800

    Update Plugin.php
    修复了一个小bug，无法获取自己配置的跳转链接

--- Plugin.php
+++ Plugin.php
@@ -1,18 +1,20 @@
 <?php
+
 /**
  * 禁止国外、港澳台IP访问，也可以自己添加国内IP黑名单
  * 被禁止的IP会默认跳转到谷歌搜索
  * @package ProhibitIP
  * @author culturesun
- * @version 1.0
- * @update: 2022.11.17
+ * @version 1.1
+ * @update: 2023.03.28
  * @link https://culturesun.site
  */
 class ProhibitIP_Plugin implements Typecho_Plugin_Interface
 {
     public static function activate()
     {
-        Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('ProhibitIP_Plugin', 'ProhibitIP');
+        Typecho_Plugin::factory('index.php')->begin = array('ProhibitIP_Plugin', 'ProhibitIP');
+        Typecho_Plugin::factory('admin/common.php')->begin = array('ProhibitIP_Plugin', 'ProhibitIP');
         return "启用ProhibitIP成功";
     }
 
@@ -22,30 +24,30 @@ class ProhibitIP_Plugin implements Typecho_Plugin_Interface
     }
 
     public static function config(Typecho_Widget_Helper_Form $form)
-    {	
+    {
+        //黑名单列表
         $ips = new Typecho_Widget_Helper_Form_Element_Textarea('ips', null, null, _t('IP黑名单列表'), _t('一行一个，支持规则:<br>以下是例子qwq<br>192.168.1.1<br>210.10.2.1-20<br>222.34.4.*<br>218.192.104.*'));
         $form->addInput($ips);
-		$location_url = new Typecho_Widget_Helper_Form_Element_Text('location_url', NULL, 'https://www.google.com/', _t('跳转链接'),'请输入标准的URL地址，IP黑名单的IP访问将会跳转至这个URL');
+        //跳转链接
+        $location_url = new Typecho_Widget_Helper_Form_Element_Text('location_url', NULL, 'https://www.google.com/', _t('跳转链接'), '请输入标准的URL地址，IP黑名单的IP访问将会跳转至这个URL');
         $form->addInput($location_url);
 
     }
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
-    {}
+    {
+    }
 
     public static function prohibitIP()
     {
-        
-
         if (ProhibitIP_Plugin::checkIP()) {
-			$location_url = trim($config->location_url) ? trim($config->location_url) : 'https://www.google.com/';
-			Typecho_Cookie::delete('__typecho_uid');
+            $location_url = \Widget\Options::alloc()->plugin('ProhibitIP')->location_url ? \Widget\Options::alloc()->plugin('ProhibitIP')->location_url : 'https://www.google.com/';
+            Typecho_Cookie::delete('__typecho_uid');
             Typecho_Cookie::delete('__typecho_authCode');
             @session_destroy();
-            header('Location: '.$location_url);
+            header('Location: ' . $location_url);
             exit;
         }
-
     }
 
     private static function checkIP()
@@ -62,14 +64,15 @@ class ProhibitIP_Plugin implements Typecho_Plugin_Interface
                 }
             }
         }
-		$lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-		$login_addr_arra = file_get_contents('http://whois.pconline.com.cn/ipJson.jsp?json=true&ip='.$ip);
-		$login_addr_arra = json_decode(trim(mb_convert_encoding($login_addr_arra, "UTF-8", "GBK")),true);	
-		$proCode1 = $login_addr_arra['proCode'];
-		if($proCode1 == '999999' || $proCode1 == '810000' || $proCode1 == '820000' || $proCode1 == '710000' || !strstr($lang, 'zh')){
-			$flag = true;
-		}
-		return $flag;
+        //检测ip和浏览器语言是否是中国
+        $lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        $login_addr_arra = file_get_contents('http://whois.pconline.com.cn/ipJson.jsp?json=true&ip=' . $ip);
+        $login_addr_arra = json_decode(trim(mb_convert_encoding($login_addr_arra, "UTF-8", "GBK")), true);
+        $proCode1 = $login_addr_arra['proCode'];
+        if ($proCode1 == '999999' || $proCode1 == '810000' || $proCode1 == '820000' || $proCode1 == '710000' || !strstr($lang, 'zh')) {
+            $flag = true;
+        }
+        return $flag;
 
     }
 
@@ -115,5 +118,4 @@ class ProhibitIP_Plugin implements Typecho_Plugin_Interface
         }
         return $ip_list;
     }
-
 }
