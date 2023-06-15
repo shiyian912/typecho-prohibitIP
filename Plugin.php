09b091433c4ec1c93c9f1571da856698df4fb68a
famasyang <50482659+famasyang@users.noreply.github.com>
2023-06-16 00:51:46 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,12 +1,11 @@
 <?php
-
 /**
- * 禁止国外、港澳台IP访问，也可以自己添加国内IP黑名单
- * 被禁止的IP会默认跳转到谷歌搜索
+ * 禁止中国大陆IP访问
+ * 被禁止的IP会显示一个提示
  * @package ProhibitIP
- * @author culturesun
- * @version 1.1
- * @update: 2023.03.28
+ * @author chatgpt-4
+ * @version 1.3
+ * @update: 2023.06.16
  * @link https://culturesun.site
  */
 class ProhibitIP_Plugin implements Typecho_Plugin_Interface
@@ -24,98 +23,68 @@ class ProhibitIP_Plugin implements Typecho_Plugin_Interface
     }
 
     public static function config(Typecho_Widget_Helper_Form $form)
-    {
-        //黑名单列表
-        $ips = new Typecho_Widget_Helper_Form_Element_Textarea('ips', null, null, _t('IP黑名单列表'), _t('一行一个，支持规则:<br>以下是例子qwq<br>192.168.1.1<br>210.10.2.1-20<br>222.34.4.*<br>218.192.104.*'));
-        $form->addInput($ips);
-        //跳转链接
-        $location_url = new Typecho_Widget_Helper_Form_Element_Text('location_url', NULL, 'https://www.google.com/', _t('跳转链接'), '请输入标准的URL地址，IP黑名单的IP访问将会跳转至这个URL');
-        $form->addInput($location_url);
-
-    }
+    {}
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
-    {
-    }
+    {}
 
     public static function prohibitIP()
     {
         if (ProhibitIP_Plugin::checkIP()) {
-            $location_url = \Widget\Options::alloc()->plugin('ProhibitIP')->location_url ? \Widget\Options::alloc()->plugin('ProhibitIP')->location_url : 'https://www.google.com/';
-            Typecho_Cookie::delete('__typecho_uid');
-            Typecho_Cookie::delete('__typecho_authCode');
-            @session_destroy();
-            header('Location: ' . $location_url);
+            header('HTTP/1.1 403 Forbidden');
+            echo '<!DOCTYPE html>
+            <html>
+            <head>
+                <title>Access Denied</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 0;
+                        padding: 0;
+                        background-color: #f0f0f0;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        height: 100vh;
+                    }
+                    .container {
+                        text-align: center;
+                    }
+                    h1 {
+                        color: #333;
+                    }
+                    p {
+                        color: #666;
+                    }
+                    @media (max-width: 600px) {
+                        h1 {
+                            font-size: 1.5em;
+                        }
+                        p {
+                            font-size: 1.2em;
+                        }
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <h1>你的访问被阻止</h1>
+                    <p>本站不提供给局域网居民服务，但是欢迎你越过长城，走向世界。</p>
+                </div>
+            </body>
+            </html>';
             exit;
         }
     }
 
     private static function checkIP()
     {
-        $flag = false;
         $request = new Typecho_Request;
         $ip = trim($request->getIp());
-        $iptable = ProhibitIP_Plugin::getAllProhibitIP();
-        if ($iptable) {
-            foreach ($iptable as $value) {
-                if (preg_match("{$value}", $ip)) {
-                    $flag = true;
-                    break;
-                }
-            }
-        }
-        //检测ip和浏览器语言是否是中国
-        $lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-        $login_addr_arra = file_get_contents('http://whois.pconline.com.cn/ipJson.jsp?json=true&ip=' . $ip);
-        $login_addr_arra = json_decode(trim(mb_convert_encoding($login_addr_arra, "UTF-8", "GBK")), true);
-        $proCode1 = $login_addr_arra['proCode'];
-        if ($proCode1 == '999999' || $proCode1 == '810000' || $proCode1 == '820000' || $proCode1 == '710000' || !strstr($lang, 'zh')) {
-            $flag = true;
-        }
-        return $flag;
-
-    }
-
-    private static function makePregIP($str)
-    {
-        if (strpos($str, "-") !== false) {
-            $aIP = explode(".", $str);
-            foreach ($aIP as $key => $value) {
-                if (strpos($value, "-") === false) {
-                    if ($key == 0) {
-                        $preg_limit .= ProhibitIP_Plugin::makePregIP($value);
-                    } else {
-                        $preg_limit .= '.' . ProhibitIP_Plugin::makePregIP($value);
-                    }
-
-                } else {
-                    $aipNum = explode("-", $value);
-                    for ($i = $aipNum[0]; $i <= $aipNum[1]; $i++) {
-                        $preg .= $preg ? "|" . $i : "[" . $i;
-                    }
-                    $preg_limit .= strrpos($preg_limit, ".", 1) == (strlen($preg_limit) - 1) ? $preg . "]" : "." . $preg . "]";
-                }
-            }
-        } else {
-            $preg_limit .= $str;
-        }
-        return $preg_limit;
-    }
-
-    private static function getAllProhibitIP()
-    {
-        $config = Typecho_Widget::widget('Widget_Options')->plugin('ProhibitIP');
-        $ips = $config->ips;
-        if ($ips) {
-            $ip_array = explode("\n", $ips);
-            foreach ($ip_array as $value) {
-                $ipaddress = ProhibitIP_Plugin::makePregIP($value);
-                $ip = str_ireplace(".", "\.", $ipaddress);
-                $ip = str_replace("*", "[0-9]{1,3}", $ip);
-                $ipaddress = "/" . trim($ip) . "/";
-                $ip_list[] = $ipaddress;
-            }
-        }
-        return $ip_list;
+        $login_addr_arra = file_get_contents('http://ip-api.com/json/'.$ip.'?lang=en');
+        $login_addr_arra = json_decode($login_addr_arra,true);
+        $countryCode = $login_addr_arra['countryCode'];
+        // 判断是否为大陆IP
+        return $countryCode == 'CN';
     }
 }
